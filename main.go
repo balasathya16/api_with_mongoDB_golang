2bac18c4067737095fab78d2ee2f852d34010ecc
rtkbala <balasathya16@yahoo.com>
2023-01-19 21:09:14 -0500

    main function

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"userapi.com/controllers"
+	"userapi.com/services"
+)
+
+var (
+	server         *gin.Engine
+	userservice    services.UserService
+	usercontroller controllers.UserController
+	ctx            context.Context
+	usercollection *mongo.Collection
+	mongoclient    *mongo.Client
+	err            error
+)
+
+func init() {
+	ctx = context.TODO()
+
+	mongoconn := options.Client().ApplyURI("mongodb://localhost:27017")
+	mongoclient, err := mongo.Connect(ctx, mongoconn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = mongoclient.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("mongo connection established")
+}
+
 func main() {
 
 }
